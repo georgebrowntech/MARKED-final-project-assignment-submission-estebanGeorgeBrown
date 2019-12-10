9f5895d18e6621787fd0e4b8cc0f1ec3fdec8130
estebanGeorgeBrown <josesteban.guzmanalvarez@georgebrown.ca>
2019-12-10 17:22:09 -0500

    Adding backend

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// *********** Include the Api routes ***********
+const userRoutes = require("./routes/users");
+const gameRoutes = require("./routes/games");
+const authRoutes = require("./routes/auth");
+
+// *********** Connect to Mongo  ***********
+console.log('Attempting to connect to mongoose');
+
+mongoose.connect('mongodb://localhost:27017/gamer-lobby', { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to Mongo database!');
+  })
+  .catch(err  => {
+    console.error('App starting error:', err.stack);
+  });
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+
+  next();
+});
+
+// ******** Setup the Api routes ***********
+app.use("/api/users", userRoutes);
+app.use("/api/games", gameRoutes);
+app.use("/api/auth", authRoutes);
+
+module.exports = app;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { UserService } from './services/user.service';
+import { GameService } from './services/game.service';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService, UserService, GameService, AuthService]
 })
-export class AppComponent {
-  title = 'gamer-lobby';
+export class AppComponent implements OnInit {
+  title = 'Friend List';
+  constructor(
+    private userService: UserService,
+    private gameService: GameService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
+  
+  ngOnInit() {
+    this.userService.updateData();
+    this.gameService.updateData();
+  }
 }
